de79eeb193d431d22abe92cc0744050b2942dd47
SamVESIT <2024.sam.alex@ves.ac.in>
2024-09-23 09:42:11 +0530

    Demo File Commit #1

---
+++ demo.java
@@ -0,0 +1,6 @@
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("This is the demo file for GitHub Demo");
+    }
+}
